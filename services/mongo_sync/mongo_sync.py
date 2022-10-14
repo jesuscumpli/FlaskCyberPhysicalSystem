cb2284ee99c4c28ca0aeffd0b0609095733b2835
jesuscumpli <jesuscumpli@uma.es>
2022-10-14 17:57:10 +0200

    init commit

--- services/mongo_sync/mongo_sync.py
+++ services/mongo_sync/mongo_sync.py
@@ -5,19 +5,34 @@ import os
 import subprocess
 
 HOST_MONGO = "192.168.1.1"
-SLEEP_TIME = 60*5
+PORT_MONGO = 27017
+HOST_LOCAL = "localhost"
+PORT_LOCAL = 27017
+DATABASE = "control_system"
+SLEEP_TIME = 60 * 5
 
-client_host = MongoClient(HOST_MONGO, 27017)
-db_host = client_host["control_system"]
+client_host = MongoClient(HOST_MONGO, PORT_MONGO)
+db_host = client_host[DATABASE]
 
-client_local = MongoClient("localhost", 27017)
-db_local = client_local["control_system"]
-
+client_local = MongoClient(HOST_LOCAL, PORT_LOCAL)
+db_local = client_local[DATABASE]
 
 while True:
-    cmd = "mongodump --host=localhost --port=27017 --out=/opt/controlSystem/services/mongo_sync/mongodump"
+    print("INICIO SERVICIO - SYNC MONGODB")
+
+    print("**** MONGODUMP ***")
+    cmd = "mongodump --host=" + HOST_LOCAL + " --port=" + str(
+        PORT_LOCAL) + " --out=/opt/controlSystem/services/mongo_sync/mongodump --db=" + DATABASE
     temp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    # get the output as a string
     output = str(temp.communicate())
-    print(output)
+    print("SALIDA MONGODUMP: " + str(output))
+
+    print("**** MONGORESTORE ***")
+    cmd = "mongorestore --host=" + HOST_MONGO + " --port=" + str(
+        PORT_MONGO) + " /opt/controlSystem/services/mongo_sync/mongodump"
+    temp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    output = str(temp.communicate())
+    print("SALIDA MONGORESTORE: " + str(output))
+
+    print("Durmiendo servicio...")
     time.sleep(SLEEP_TIME)
