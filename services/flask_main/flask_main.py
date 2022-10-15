2efa7db6c70fe3f9bcb0cf3769ad7a83131657ca
jesuscumpli <jesuscumpli@uma.es>
2022-10-15 17:53:32 +0200

    init commit

--- services/flask_main/flask_main.py
+++ services/flask_main/flask_main.py
@@ -1,21 +1,4 @@
 import sys
 sys.path.append("/opt/controlSystem")
 
-import logging
-logging.basicConfig(level=logging.INFO)
-from flask import Flask
-from routes import *
-import datetime
-
-logging.info("Starting flask service...")
-app = Flask(__name__)
-app.register_blueprint(routes)
-app.secret_key = '6ee9a71761572d9f91dc2067da170889'
-
-@app.template_filter()
-def format_datetime(date_value):
-    date = datetime.datetime.strptime(date_value, "%Y-%m-%dT%H:%M:%S.%fZ")
-    return date.strftime("%Y-%m-%d %H:%M:%S")
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+import main
