62589389d98a96b91b4ddbccc3865481c757a2b5
jesuscumpli <jesuscumpli@uma.es>
2022-10-15 16:12:39 +0200

    init commit

---
+++ services/listen_data/listen_data.py
@@ -0,0 +1,25 @@
+from socketserver import TCPServer, StreamRequestHandler
+import socket
+import logging
+logging.basicConfig(level=logging.INFO)
+
+HOST, PORT = "localhost", 8887
+
+class Handler(StreamRequestHandler):
+    def handle(self):
+        self.data = self.rfile.readline().strip()
+        logging.info("From <%s>: %s" % (self.client_address, self.data))
+        self.wfile.write(self.data.upper() + "\r\n".encode("utf-8"))
+
+
+class Server(TCPServer):
+    SYSTEMD_FIRST_SOCKET_FD = 3
+
+    def __init__(self, server_address, handler_cls):
+        TCPServer.__init__(self, server_address, handler_cls, bind_and_activate=False)
+        self.socket = socket.fromfd(self.SYSTEMD_FIRST_SOCKET_FD, self.address_family, self.socket_type)
+
+
+if __name__ == "__main__":
+    server = Server((HOST, PORT), Handler)
+    server.serve_forever()
--- services/mongo_sync/mongo_sync.py
+++ services/mongo_sync/mongo_sync.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from pymongo import MongoClient
 import os
 import subprocess
+import logging
+logging.basicConfig(level=logging.INFO)
 
 HOST_MONGO = "192.168.1.1"
 PORT_MONGO = 27017
@@ -11,28 +13,31 @@ PORT_LOCAL = 27017
 DATABASE = "control_system"
 SLEEP_TIME = 60 * 5
 
-client_host = MongoClient(HOST_MONGO, PORT_MONGO)
-db_host = client_host[DATABASE]
-
-client_local = MongoClient(HOST_LOCAL, PORT_LOCAL)
-db_local = client_local[DATABASE]
+# client_host = MongoClient(HOST_MONGO, PORT_MONGO)
+# db_host = client_host[DATABASE]
+#
+# client_local = MongoClient(HOST_LOCAL, PORT_LOCAL)
+# db_local = client_local[DATABASE]
 
 while True:
-    print("INICIO SERVICIO - SYNC MONGODB")
+    try:
+        logging.info("INICIO SERVICIO - SYNC MONGODB")
 
-    print("**** MONGODUMP ***")
-    cmd = "mongodump --host=" + HOST_LOCAL + " --port=" + str(
-        PORT_LOCAL) + " --out=/opt/controlSystem/services/mongo_sync/mongodump --db=" + DATABASE
-    temp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    output = str(temp.communicate())
-    print("SALIDA MONGODUMP: " + str(output))
+        logging.info("**** MONGODUMP ***")
+        cmd = "mongodump --host=" + HOST_LOCAL + " --port=" + str(
+            PORT_LOCAL) + " --out=/opt/controlSystem/services/mongo_sync/mongodump --db=" + DATABASE
+        temp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        output = str(temp.communicate())
+        logging.info("SALIDA MONGODUMP: " + str(output))
 
-    print("**** MONGORESTORE ***")
-    cmd = "mongorestore --host=" + HOST_MONGO + " --port=" + str(
-        PORT_MONGO) + " /opt/controlSystem/services/mongo_sync/mongodump"
-    temp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    output = str(temp.communicate())
-    print("SALIDA MONGORESTORE: " + str(output))
+        logging.info("**** MONGORESTORE ***")
+        cmd = "mongorestore --host=" + HOST_MONGO + " --port=" + str(
+            PORT_MONGO) + " /opt/controlSystem/services/mongo_sync/mongodump"
+        temp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        output = str(temp.communicate())
+        logging.info("SALIDA MONGORESTORE: " + str(output))
+    except Exception as e:
+        logging.exception("ERROR: " + str(e))
 
-    print("Durmiendo servicio...")
+    logging.info("Durmiendo servicio...")
     time.sleep(SLEEP_TIME)
