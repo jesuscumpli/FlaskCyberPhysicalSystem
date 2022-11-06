de8148588d4d2fab690dffde8859011152a35eba
jesuscumpli <jesuscumpli@uma.es>
2022-11-06 23:14:36 +0100

    init commit

--- routes/logsDeviceRoutes.py
+++ routes/logsDeviceRoutes.py
@@ -19,4 +19,8 @@ def logs_device(device_id):
         return redirect("/login")
     device = get_device_by_id(device_id)
     logs_device = get_all_logs_from_device(device_id)
+    for log in logs_device:
+        log = json.loads(log)
+        log = log["message"]
+
     return render_template("logs_device.html", device=device, logs_device=logs_device)
